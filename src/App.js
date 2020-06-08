d7d1f4f5c4acd18a118c8387d6d375ef8defea29
doanpham98 <meodien.tb@gmail.com>
2020-06-08 20:25:55 +0700

    complete app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+const [selected,setSelected] = useState(0);
+const [pokemons, setPokemons] = useState([
+  {pokeImg: 'http://pngimg.com/uploads/pokemon/pokemon_PNG108.png', point: 0},
+  {pokeImg: 'http://pngimg.com/uploads/pokemon/pokemon_PNG118.png', point: 0},
+  {pokeImg: 'http://pngimg.com/uploads/pokemon/pokemon_PNG128.png', point: 0},
+  {pokeImg: 'http://pngimg.com/uploads/pokemon/pokemon_PNG138.png', point: 0},
+  {pokeImg: 'http://pngimg.com/uploads/pokemon/pokemon_PNG158.png', point: 0}
+])
+   const mostVote= React.useMemo(()=>{
+    console.log('dâta');
+    let pointArr=pokemons.map(pokemon=>pokemon.point);
+    let max= Math.max(...pointArr);
+    return pokemons.find(pokemon=>pokemon.point===max);
+   },[pokemons])
+   const random = ()=>{
+    const newSelected = Math.floor(Math.random()*pokemons.length);
+    setSelected(newSelected!==selected?newSelected:
+      newSelected===selected&&newSelected===pokemons.length-1?newSelected-1:
+      newSelected+1);
+  }
+  const vote=(selected)=>{
+      setPokemons(pokemons.map(pokemon=>
+      pokemons.indexOf(pokemon)===selected?
+      {...pokemon,point:pokemon.point+1}:pokemon
+      ))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>pokemon of the day</h1>
+    <div className="img-box">
+    <img src={pokemons[selected].pokeImg} alt="pokemon"/>
+    <h2>has voted {pokemons[selected].point}</h2>
+    </div>
+      <button onClick={()=>vote(selected)}>Vote</button>
+      <button onClick={random}>Next pokemon</button>
+    <div className="img-box">
+       <h1>
+         pokemon with most vote
+       </h1>
+       <img src={mostVote.pokeImg} alt="pokemon"/>
+         <h2>has voted {mostVote.point}</h2>
+    </div>
     </div>
   );
 }
