8372fafe9a5e99b97bcd161025e050a3fa730c37
doanpham98 <meodien.tb@gmail.com>
2020-06-08 21:22:31 +0700

    Updates

---
+++ precache-manifest.2a6345f1ffc39454b3283b54273fe6b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d5c283e47f608c12b066e0cb5582e6",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "8628204603870e3006bf",
+    "url": "/pokemon/static/css/main.c6e3991c.chunk.css"
+  },
+  {
+    "revision": "0df2adc8f53f35612cf8",
+    "url": "/pokemon/static/js/2.1c1d60f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon/static/js/2.1c1d60f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8628204603870e3006bf",
+    "url": "/pokemon/static/js/main.e849a509.chunk.js"
+  },
+  {
+    "revision": "9570cfbd4222c6d323a6",
+    "url": "/pokemon/static/js/runtime-main.647f550a.js"
+  }
+]);
